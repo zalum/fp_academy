336e3a2f7e3690541898be736af5ceb161ddde42
florin <Florin.Zalum@zooplus.com>
2016-01-22 15:42:36 +0100

    green

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,9 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class Task1 {
 
-    public List<Integer> doIt(int noNumbers){
-        return new ArrayList<Integer>();
+    public List<Integer> doIt(int noNumbers) {
+        List<Integer> result =
+                IntStream.iterate(1, value -> value + 1).limit(noNumbers).collect((Supplier<List<Integer>>) ArrayList::new,
+                                                                                  List::add,
+                                                                                  (left, right) -> left.addAll(right));
+        return result;
     }
 }
