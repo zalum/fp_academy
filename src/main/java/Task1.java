a297ecc08217e9fcb31af61f4415f104a8bc1ddc
florin <Florin.Zalum@zooplus.com>
2016-01-22 15:44:52 +0100

    refactor

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class Task1 {
 
     public List<Integer> doIt(int noNumbers) {
-        List<Integer> result =
-                IntStream.iterate(1, value -> value + 1).limit(noNumbers).collect((Supplier<List<Integer>>) ArrayList::new,
-                                                                                  List::add,
-                                                                                  (left, right) -> left.addAll(right));
-        return result;
+        IntStream stream = IntStream.iterate(1, value -> value + 1).limit(noNumbers);
+        return collectToList(stream);
+    }
+
+    private List<Integer> collectToList(IntStream stream) {
+        return stream.collect((Supplier<List<Integer>>) ArrayList::new,
+                              List::add,
+                              (left, right) -> left.addAll(right));
     }
 }
