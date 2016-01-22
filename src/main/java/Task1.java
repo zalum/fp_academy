6ee2ebd230c53bf238bb9da65ec4d328a6789e9f
florin <Florin.Zalum@zooplus.com>
2016-01-22 15:15:59 +0100

    red

---
+++ src/main/java/Task1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+
+    public List<Integer> doIt(int noNumbers){
+        return new ArrayList<Integer>();
+    }
+}
---
+++ test/main/java/Task1Test.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by florin on 1/22/16.
+ */
+public class Task1Test {
+
+    private Task1 task1 = new Task1();
+
+    @Test
+    public void shouldReturnNumbers() throws Exception {
+        //given
+        int numberOfNumbers = 25;
+        //when
+        List<Integer> result = task1.doIt(numberOfNumbers);
+
+        //then
+        assertThat(result).hasSize(numberOfNumbers);
+    }
+}
